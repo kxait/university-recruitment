fdbeba65f5998c977e4ece6c11a9629007e0ad5f
Tomasz Osika <tomasz.osika@protonmail.com>
2023-01-21 22:30:29 +0100

    add caret

--- app/routes/recruitments/$userId.tsx
+++ app/routes/recruitments/$userId.tsx
@@ -100,6 +100,20 @@ export default function MyRecruitment() {
                           className={`btn${status.classSuffix} btn m-1`}
                         >
                           {status.status}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="h-6 w-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                            />
+                          </svg>
                         </label>
                         <ul
                           tabIndex={0}
